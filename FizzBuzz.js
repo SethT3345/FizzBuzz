8077c6a1c9604be98162c81c6546ab62db224f3d
Seth Tomlinson <sjt2908@gmail.com>
2025-09-17 08:55:09 -0600

    Updated Html with FizzBuzz Button

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,16 +1,21 @@
-function fizzBuzz(amount){
-    for(let i = 0; i < amount; i++){
-        if(i % 3 == 0 && i % 5 == 0){
-            console.log("FizzBuzz")
+function fizzBuzz() {
+      let result = [];
+      //Makes empty array
+      let amount = parseInt(document.getElementById("number").value);
+      //makes variabke amount = the number the user enters
+      for (let i = 1; i <= amount; i++) {
+    //makes a loop function that limits at the usrs amount entered
+        if (i % 3 === 0 && i % 5 === 0) {
+          result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+          result.push("Fizz");
+        } else if (i % 5 === 0) {
+          result.push("Buzz");
+        } else {
+          result.push(i);
         }
-        else if(i % 3 == 0) {
-            console.log("Fizz")
-        }
-        else if(i % 5 == 0) {
-            console.log("Buzz")
-        }
-        else{
-            console.log(i)
-        }
-    }
+      }
+      //determines if number is fizz or buzz or fizzbuzz, then pushes that into the result array
+      document.getElementById("output").innerHTML = result.join("<br>");
+      //joins elemnts inside the array together, with the lines breaking, then pastes it into the div i made with the id output
 }
